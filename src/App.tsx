46a15d69c7f326a6bd3517f60c7e6ad6a5376d96
Piyush Nanwani <pnanwani61@gmail.com>
2026-07-18 12:06:54 +0530

    map function example

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-function Header({data}) {
-  return (<h1>{data}</h1>); // data is a variable, we will curl 
+const classicMovies = [
+  "The Godfather",
+  "The Shawshank Redemption",
+  "Inception",
+  "Pulp Fiction",
+  "The Dark Knight"
+];
+
+function Header({ data, number }: { data: string; number: number }) {
+  return (
+    <h1>
+      {data} {number}{" "}
+    </h1>
+  ); // data is a variable, we will curl
 }
 
 function App() {
-  
   return (
     <>
-      <Header data="Heading 1" />
-      <Header data="Heading 2" />
-      <Header data="Heading 3" />
+      {classicMovies.map((movie, idx) => {
+        return <Header data={movie} number={idx} />;
+      })}
+
+      {/* <Header data="Heading 1" number={12} />
+      <Header data="Heading 2" number={23} />
+      <Header data="Heading 3" number={99} /> */}
       <h1>this is a functional component (Parent)</h1>
     </>
   );
