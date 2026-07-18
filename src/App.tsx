d7ee6d59d8f657b4eec7ab7f8a6cd5932023187b
Piyush Nanwani <pnanwani61@gmail.com>
2026-07-18 12:40:06 +0530

    added type User

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,22 @@
-function BadList() {
-  const items = ['Apple', 'Banana', 'Mango'];
 
-  return (
-    <ul>
-      {items.map((fruit, index) => {
-        return (
-          <li>{fruit} </li>
-        )
-      })}
-    </ul>
-  )
+interface User {
+  id: number;
+  name: string;
+  email: string
 }
-
-function BadList2() {
-  const items = ["Apple", "Banana", "Mango"];
-
-  return (
-    <ul>
-      {items.map((fruit, index) => {
-        return <li key={index} >{fruit} </li>;
-      })}
-    </ul>
-  );
-}
-
-function GoodList() {
-  const items = [{ name: "Apple", id: 1}, {name: "Banana", id: 4}, {name: "Mango", id: 10}];
-
-  return (
-    <ul>
-      {items.map((fruit, index) => {
-        return <li key={fruit.id}>{fruit.name} </li>;
-      })}
-    </ul>
-  );
-}
-
-
 
 function App() {
+  const user: User | null = {id: 1, name: "Rahul", email:"rahul@gmail.com"}
+  // const user: User | null = null;  
+  
+  if (user == null) {
+    return <h1>Please login to continue</h1>
+  }
+
   return (
     <>
-      <BadList />
-      <BadList2 />
-      <GoodList />
+    <h1>Hi {user.name}!</h1>
+    <h2>{user.id === 1 ? "You are the 1st user!" : ""}</h2>
     </>
   );
 }
